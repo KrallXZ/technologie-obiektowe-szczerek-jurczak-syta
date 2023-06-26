8fe48896f42beb8833efb847bf567c43b14d5768
michal1534 <michal.szczerek@silevis.com>
2023-06-26 22:03:05 +0200

    Add filter option to mongodb and put data into table

--- src/pages/mongodb/query-details/index.tsx
+++ src/pages/mongodb/query-details/index.tsx
@@ -1,82 +1,143 @@
-import { NextPage } from "next";
-import Layout from "~/components/layout/layout";
-import { useRouter } from "next/router";
-import React from "react";
-import { api } from "~/utils/api";
-import { Button, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import { NextPage } from 'next';
+import Layout from '~/components/layout/layout';
+import { useRouter } from 'next/router';
+import React, { useEffect, useMemo } from 'react';
+import { api } from '~/utils/api';
+import { Button, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
 
 const MongodbQueryDetails: NextPage = () => {
-  const router = useRouter();
+    const router = useRouter();
 
-  const [isEnabledQuerySpecificResults, setIsEnabledQuerySpecificResults] = React.useState(false);
-  const [filterName, setFilterName] = React.useState('');
-  const [filterValue, setFilterValue] = React.useState('');
+    const [filterName, setFilterName] = React.useState('');
+    const [filterValue, setFilterValue] = React.useState('');
+    const [columnNames, setColumnNames] = React.useState<Set<string>>(new Set());
+    const [filteredColumnNames, setFilteredColumnNames] = React.useState<Set<string>>(new Set());
 
-  const { connectionString, databaseName, collectionName, isEnabledQueryResults } = router.query;
+    const { connectionString, databaseName, collectionName, isEnabledQueryResults } = router.query;
 
-  const queryResults = api.mongodb.getDatabaseResults.useQuery( {connectionString, databaseName, collectionName}, {enabled: Boolean(isEnabledQueryResults)});
-  const querySpecificResults = api.mongodb.getSpecificDatabaseResults.useQuery( {connectionString, databaseName, collectionName, filterName, filterValue}, {enabled: isEnabledQuerySpecificResults});
+    const queryResults = api.mongodb.getDatabaseResults.useQuery(
+        { connectionString, databaseName, collectionName },
+        { enabled: Boolean(isEnabledQueryResults) }
+    );
+    const querySpecificResults = api.mongodb.getSpecificDatabaseResults.useMutation();
 
-  const onFilterValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFilterValue(event.target.value)
-  }
+    const onFilterValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilterValue(event.target.value);
+    };
 
-  const onFilterNameChange = (event: SelectChangeEvent<typeof filterName>) => {
-    setFilterName(event.target.value);
-  }
+    const onFilterNameChange = (event: SelectChangeEvent<typeof filterName>) => {
+        setFilterName(event.target.value);
+    };
 
-  const selectSpecificResults = () => {
-    setIsEnabledQuerySpecificResults(true);
-  }
-/*  console.log(connectionString, databaseName, collectionName)*/
+    const selectSpecificResults = () => {
+        querySpecificResults.mutate({
+            connectionString,
+            databaseName,
+            collectionName,
+            filterName,
+            filterValue,
+        });
 
-  return (
-    <>
-      <Layout>
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            MongoDB Query Builder
-          </h1>
-          <h2>All results:</h2>
-          <ul>
-            {queryResults.data?.map((item: any) => {
-              return <li key={item}>{JSON.stringify(item)}</li>
-            })}
-          </ul>
+        setFilteredColumnNames(new Set());
+    };
 
-          <div>
-            <Select
-              value={filterName}
-              placeholder={'Select filter name'}
-              onChange={onFilterNameChange}
-              sx={{ minWidth: 120 }}>
-              {queryResults.data?.map((resultKey) => (
-                /*TODO Find all keys (https://www.mongodb.com/docs/manual/reference/method/KeyVault.getKeys/#mongodb-method-KeyVault.getKeys)*/
-                <MenuItem
-                  key={resultKey._id}
-                  value={resultKey._id}>
-                  {resultKey._id}
-                </MenuItem>
-              ))}
-            </Select>
-            <TextField variant="outlined" label="Filter value" value={filterValue}
-                       onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                         onFilterValueChange(event)
-                       }}>
-            </TextField>
+    useEffect(() => {
+        if (queryResults?.data) {
+            queryResults.data.forEach((item) => {
+                setColumnNames((prevValues) => new Set([...prevValues, ...Object.keys(item)]));
+            });
+        }
+    }, [queryResults.data]);
 
-            <Button onClick={ () => {
-               selectSpecificResults;
-            }} variant="outlined">
-              Select specific results
-              <KeyboardArrowRightIcon />
-            </Button>
-          </div>
-        </div>
-      </Layout>
-    </>
-  );
+    useEffect(() => {
+        if (querySpecificResults?.data) {
+            querySpecificResults.data.forEach((item) => {
+                setFilteredColumnNames((prevValues) => new Set([...prevValues, ...Object.keys(item)]));
+            });
+        }
+    }, [querySpecificResults.data]);
+
+    const resultColumns: GridColDef[] = useMemo(() => {
+        if (filteredColumnNames.size > 0) {
+            return Array.from(filteredColumnNames).map((column) => {
+                return {
+                    field: column,
+                    headerName: column,
+                    width: 150,
+                };
+            });
+        } else {
+            return Array.from(columnNames).map((column) => {
+                return {
+                    field: column,
+                    headerName: column,
+                    width: 150,
+                };
+            });
+        }
+    }, [columnNames, filteredColumnNames]);
+    const resultRows: GridRowsProp = useMemo(() => {
+        if (querySpecificResults.data?.length > 0) {
+            return querySpecificResults.data?.map((row, index) => ({ ...row, id: index })) || [];
+        } else {
+            return queryResults.data?.map((row, index) => ({ ...row, id: index })) || [];
+        }
+    }, [queryResults.data, querySpecificResults.data]);
+
+    return (
+        <>
+            <Layout>
+                <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+                    <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                        MongoDB Query Builder
+                    </h1>
+
+                    <div>
+                        {queryResults.isLoading ? (
+                            <div>Loading...</div>
+                        ) : queryResults.data ? (
+                            <DataGrid columns={resultColumns} rows={resultRows} />
+                        ) : null}
+                    </div>
+
+                    <div>
+                        <Select
+                            value={filterName}
+                            placeholder={'Select filter name'}
+                            onChange={onFilterNameChange}
+                            sx={{ minWidth: 120 }}
+                        >
+                            {Array.from(columnNames).map((columnName) => (
+                                <MenuItem key={columnName} value={columnName}>
+                                    {columnName}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                        <TextField
+                            variant="outlined"
+                            label="Filter value"
+                            value={filterValue}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                onFilterValueChange(event);
+                            }}
+                        ></TextField>
+
+                        <Button
+                            onClick={() => {
+                                selectSpecificResults();
+                            }}
+                            variant="outlined"
+                        >
+                            Select specific results
+                            <KeyboardArrowRightIcon />
+                        </Button>
+                    </div>
+                </div>
+            </Layout>
+        </>
+    );
 };
 
 export default MongodbQueryDetails;
--- src/server/api/routers/mongodb/mongodb.ts
+++ src/server/api/routers/mongodb/mongodb.ts
@@ -1,79 +1,89 @@
-import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { MongoClient } from "mongodb";
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
+import { MongoClient } from 'mongodb';
 
-const getClient = async (connectionString: string) =>  {
+const getClient = async (connectionString: string) => {
     const client = new MongoClient(connectionString);
     const connection = await client.connect();
 
-  return connection;
-}
+    return connection;
+};
 
-const getDatabase = async (connectionString: string, databaseName: string) =>  {
+const getDatabase = async (connectionString: string, databaseName: string) => {
     const client = await getClient(connectionString);
 
     if (!client) {
-      throw new Error("No client");
+        throw new Error('No client');
     }
 
-    return client.db(databaseName)
-
-}
+    return client.db(databaseName);
+};
 
 export const mongodbRouter = createTRPCRouter({
-  getDatabaseList: publicProcedure.input(z.string()).query(async ({ input: connectionString }) => {
-    try {
-      const client = await getClient(connectionString)
-      return client.db().admin().listDatabases();
-    } catch (error) {
-      console.log(error);
-    }
-  }),
+    getDatabaseList: publicProcedure.input(z.string()).query(async ({ input: connectionString }) => {
+        try {
+            const client = await getClient(connectionString);
+            return client.db().admin().listDatabases();
+        } catch (error) {
+            console.log(error);
+        }
+    }),
 
-  getCollectionList: publicProcedure.input(z.object({
-    connectionString: z.string(),
-    databaseName: z.string(),
-  })).query(async ({  input: { connectionString, databaseName } }) => {
-    try {
-      const db = await getDatabase(connectionString, databaseName);
+    getCollectionList: publicProcedure
+        .input(
+            z.object({
+                connectionString: z.string(),
+                databaseName: z.string(),
+            })
+        )
+        .query(async ({ input: { connectionString, databaseName } }) => {
+            try {
+                const db = await getDatabase(connectionString, databaseName);
 
-      return db.listCollections().toArray();
-    } catch (error) {
-      console.log(error);
-    }
-  }),
+                return db.listCollections().toArray();
+            } catch (error) {
+                console.log(error);
+            }
+        }),
 
-  getDatabaseResults: publicProcedure.input(z.object({
-    connectionString: z.string(),
-    databaseName: z.string(),
-    collectionName: z.string(),
-  })).query(async ({ input: { connectionString, collectionName, databaseName } }) => {
-    try {
-      const db = await getDatabase(connectionString, databaseName);
-      const collection = db.collection(collectionName);
+    getDatabaseResults: publicProcedure
+        .input(
+            z.object({
+                connectionString: z.string(),
+                databaseName: z.string(),
+                collectionName: z.string(),
+            })
+        )
+        .query(async ({ input: { connectionString, collectionName, databaseName } }) => {
+            try {
+                const db = await getDatabase(connectionString, databaseName);
+                const collection = db.collection(collectionName);
 
-      return collection.find().toArray();
-    } catch (error) {
-      console.log(error);
-    }
-  }),
+                return collection.find().toArray();
+            } catch (error) {
+                console.log(error);
+            }
+        }),
 
-  getSpecificDatabaseResults: publicProcedure.input(z.object({
-    connectionString: z.string(),
-    databaseName: z.string(),
-    collectionName: z.string(),
-    filterName: z.string().optional(),
-    filterValue: z.string().optional(),
-  })).query(async ({ input: { connectionString, collectionName, databaseName, filterName, filterValue } }) => {
-    try {
-      const db = await getDatabase(connectionString, databaseName);
-      const collection = db.collection(collectionName);
-
-      return collection.find({ filterName: filterValue }).toArray();
-    } catch (error) {
-      console.log(error);
-    }
-  }),
+    getSpecificDatabaseResults: publicProcedure
+        .input(
+            z.object({
+                connectionString: z.string(),
+                databaseName: z.string(),
+                collectionName: z.string(),
+                filterName: z.string().optional(),
+                filterValue: z.string().optional(),
+            })
+        )
+        .mutation(async ({ input: { connectionString, collectionName, databaseName, filterName, filterValue } }) => {
+            try {
+                const db = await getDatabase(connectionString, databaseName);
+                const collection = db.collection(collectionName);
+                return collection.find({ [filterName]: filterValue }).toArray();
+            } catch (error) {
+                console.log(error);
+            }
+        }),
 });
 
 export type mongodbRouter = typeof mongodbRouter;
